33c349dba948259a7919d6e0c7acc580acf30f83
Diego jimenes <lligaskate@gmail.com>
2019-09-01 12:03:49 -0300

    redux redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './screens/login'
+
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import store from './redux/store/store'
+
 export default class Home extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <Provider store={createStore(store, {}, applyMiddleware(ReduxThunk))}>
           <Route path="/" exact component={Login} />
-        </div>
+        </Provider>
       </Router>
     );
   }
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import firebase from "../firebase_keys";
-export default class Login extends Component {
- render() {
-return(
-    <div>
-        <button
-            onClick={() => {
-                firebase
-                .auth()
-                .signInAnonymously()
-                .then(user => console.log(user))
-                .catch(function(error) {
-                    console.log(error);
-                });
-            }}
-        >
-            login
-        </button>
-        <button
-            onClick={() => {
-                firebase
-                .auth()
-                .signOut()
-                .then(function() {
-                    console.log("deslogado");
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-            }}
-        >
-            logOut
-        </button>
-    </div>
-);
+class Login extends Component {
+    
+    render() {
+        console.log(this.props)
+        return(
+            <div>
+                <button
+                    onClick={() => {
+                        firebase
+                        .auth()
+                        .signInAnonymously()
+                        .then(user => console.log(user))
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                    }}
+                >
+                    login
+                </button>
+                <button
+                    onClick={() => {
+                        firebase
+                        .auth()
+                        .signOut()
+                        .then(function() {
+                            console.log("deslogado");
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                    }}
+                >
+                    logOut
+                </button>
+            </div>
+        );
+    }
 }
-}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+export default connect(mapStateToProps)(Login)
