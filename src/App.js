442349098b7e5803ff0536c56978639eb44efc7b
Diego jimenes <lligaskate@gmail.com>
2019-09-01 11:51:17 -0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React, { Component } from "react";
-import firebase from "../firebase_keys";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './screens/login'
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <button
-          onClick={() => {
-            firebase
-              .auth()
-              .signInAnonymously()
-              .then(user => console.log(user))
-              .catch(function(error) {
-                console.log(error);
-              });
-          }}
-        >
-          login
-        </button>
-        <button
-          onClick={() => {
-            firebase
-              .auth()
-              .signOut()
-              .then(function() {
-                console.log("deslogado");
-              })
-              .catch(function(error) {
-                console.log(error);
-              });
-          }}
-        >
-          logOut
-        </button>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Login} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/screens/login.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import firebase from "../firebase_keys";
+export default class Login extends Component {
+ render() {
+return(
+    <div>
+        <button
+            onClick={() => {
+                firebase
+                .auth()
+                .signInAnonymously()
+                .then(user => console.log(user))
+                .catch(function(error) {
+                    console.log(error);
+                });
+            }}
+        >
+            login
+        </button>
+        <button
+            onClick={() => {
+                firebase
+                .auth()
+                .signOut()
+                .then(function() {
+                    console.log("deslogado");
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+            }}
+        >
+            logOut
+        </button>
+    </div>
+);
+}
+}
